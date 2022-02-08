ef3edc970fd144509df8a691cbc364f485f2d050
Israel Santos <48060388+DevIsraelSantos@users.noreply.github.com>
2022-02-08 12:52:51 -0300

    Add jest configs

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,3 @@
 const routes = require("express").Router();
-const { User } = require('./app/models')
-
-//Status de online
-routes.get("/", (req, res) => { return res.json({ status: "Online" }); });
-
-
 
 module.exports = routes;
