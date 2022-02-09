85985d124115d49e49d1a072d3baf4226203a47f
Israel Santos <48060388+DevIsraelSantos@users.noreply.github.com>
2022-02-09 16:24:10 -0300

    Finalizado

--- jest.config.js
+++ jest.config.js
@@ -5,17 +5,34 @@
 
 module.exports = {
 
+  //-----------------------------------------------
   // Stop running tests after `n` failures
   //Para na primeira falha
   bail: true,
 
+  //-----------------------------------------------
   // Automatically clear mock calls, instances and results before every test
   clearMocks: true,
 
+  //-----------------------------------------------
+  //Habilitar Analise de test
+  collectCoverage: true,
+  collectCoverageFrom: [
+      //Testar tudo de src
+      'src/**',
+
+      //Não testar migrations e server
+      '!src/database/migrations/**', '!src/server.js'
+    ],
+    
+  coverageDirectory: "__tests__/coverage",
+
+  //-----------------------------------------------
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
 
-  // The glob patterns Jest uses to detect test files
+  //-----------------------------------------------
+  //Arquivos de test
    testMatch: [
      "**/__tests__/**/*.test.js?(x)",
    ],
